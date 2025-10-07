795984452b36c5c35a474ba8640e525ace6d6867
Kyrylo Karnyshov <kyrylo.karnyshov@gmail.com>
2025-10-07 17:30:49 +0300

    Added test drafts and page object

--- src/pages/checkout_info_page.py
+++ src/pages/checkout_info_page.py
@@ -7,3 +7,57 @@ class CheckoutInfoPage(BasePage):
     def __init__(self, page: Page) -> None:
         super().__init__(page)
         self.page = page
+        self.page_title = page.locator("//span[@class=\"title\"]")
+        self.cancel_button = page.locator("//button[@id=\"cancel\"]")
+        self.continue_button = page.locator("//input[@id=\"continue\"]")
+        self.first_name_field = page.locator("//input[@id=\"first-name\"]")
+        self.last_name_field = page.locator("//input[@id=\"last-name\"]")
+        self.postal_code_field = page.locator("//input[@id=\"postal-code\"]")
+
+    def verify_first_name_field(self):
+        logger.info("Checking First Name field: visibility, placeholder")
+        expect(self.first_name_field).to_be_visible()
+        expect(self.first_name_field).to_be_editable()
+        expect(self.first_name_field).to_have_attribute("placeholder", "First Name")
+
+    def verify_last_name_field(self):
+        logger.info("Checking Last Name field: visibility, placeholder")
+        expect(self.last_name_field).to_be_visible()
+        expect(self.last_name_field).to_be_editable()
+        expect(self.last_name_field).to_have_attribute("placeholder", "Last Name")
+
+    def verify_postal_code_field(self):
+        logger.info("Checking Postal Code field: visibility, placeholder")
+        expect(self.postal_code_field).to_be_visible()
+        expect(self.postal_code_field).to_be_editable()
+        expect(self.postal_code_field).to_have_attribute("placeholder", "Zip/Postal Code")
+
+    def verify_continue_button(self):
+        logger.info("Checking Continue button: visibility, clickable")
+        expect(self.continue_button).to_be_visible()
+        expect(self.continue_button).to_be_enabled()
+
+    def verify_cancel_button(self):
+        logger.info("Checking Cancel button: visibility, clickable")
+        expect(self.cancel_button).to_be_visible()
+        expect(self.cancel_button).to_be_enabled()
+
+    def cancel_checkout(self):
+        logger.info("Cancelling checkout, opening Cart page")
+        self.cancel_button.click()
+
+    def continue_checkout(self):
+        logger.info("Continue checkout, opening Checkout Overview page")
+        self.continue_button.click()
+
+    def input_first_name(self, text):
+        logger.info(f"Typing a First Name: {text}")
+        self.first_name_field.fill(text)
+
+    def input_last_name(self, text):
+        logger.info(f"Typing a Last Name: {text}")
+        self.last_name_field.fill(text)
+
+    def input_postal_code(self, text):
+        logger.info(f"Typing a Postal Code: {text}")
+        self.postal_code_field.fill(text)
---
+++ tests/test_checkout_information_page.py
@@ -0,0 +1,95 @@
+from playwright.sync_api import expect
+from tests.conftest import page_manager as pm
+from tests.conftest import open_checkout_info_page as checkout
+
+class TestCheckoutInfo:
+    def test_basic_state(self, pm, checkout):
+        pm.cart_page.verify_burger_menu_closed()
+        pm.cart_page.verify_copyright()
+        pm.cart_page.verify_footer_twitter()
+        pm.cart_page.verify_footer_facebook()
+        pm.cart_page.verify_footer_linkedin()
+        pm.checkout_info_page.verify_last_name_field()
+        pm.checkout_info_page.verify_last_name_field()
+        pm.checkout_info_page.verify_postal_code_field()
+        pm.checkout_info_page.verify_cancel_button()
+        pm.checkout_info_page.verify_continue_button()
+        expect(pm.checkout_info_page.page_title).to_have_text("Checkout: Your Information")
+        expect(pm.checkout_info_page.shopping_cart_count).to_have_text("1")
+        expect(pm.checkout_info_page.shopping_cart).to_be_visible()
+
+    def test_footer_twitter(self, pm, checkout):
+        pm.checkout_info_page.verify_footer_twitter()
+        pm.twitter = pm.checkout_info_page.click_footer_button(pm.checkout_info_page.footer_twitter)
+        expect(pm.twitter.page).to_have_url("https://x.com/saucelabs")
+        expect(pm.twitter.page).to_have_title("Sauce Labs (@saucelabs) / X")
+
+    def test_footer_facebook(self, pm, checkout):
+        pm.checkout_info_page.verify_footer_facebook()
+        pm.facebook = pm.checkout_info_page.click_footer_button(pm.checkout_info_page.footer_facebook)
+        expect(pm.facebook_page.page).to_have_url("https://www.facebook.com/saucelabs")
+        expect(pm.facebook_page.page).to_have_title("Sauce Labs | Facebook")
+
+    def test_footer_linkedin(self, pm, checkout):
+        pm.checkout_info_page.verify_footer_linkedin()
+        pm.linkedin = pm.checkout_info_page.click_footer_button(pm.checkout_info_page.footer_linkedin)
+        expect(pm.linkedin_page.page).to_have_url("https://www.linkedin.com/company/sauce-labs/")
+        expect(pm.linkedin_page.page).to_have_title("Sauce Labs | LinkedIn")
+
+    def test_burger_menu(self, pm, checkout):
+        pm.checkout_info_page.verify_burger_menu_closed()
+        pm.checkout_info_page.open_burger_menu()
+        pm.checkout_info_page.verify_burger_menu_opened()
+        pm.checkout_info_page.close_burger_menu()
+        pm.checkout_info_page.verify_burger_menu_closed()
+
+    #TODO: add check if menu already opened?
+    def test_burger_menu_all_items(self, pm, checkout):
+        pm.checkout_info_page.open_burger_menu()
+        expect(pm.checkout_info_page.burger_menu_all_items).to_have_attribute("href","#")
+        pm.checkout_info_page.burger_menu_all_items.click()
+
+    def test_burger_menu_about(self, pm, checkout):
+        pm.checkout_info_page.open_burger_menu()
+        expect(pm.checkout_info_page.burger_menu_about).to_have_attribute("href","https://saucelabs.com/")
+        pm.checkout_info_page.open_about_page()
+        expect(pm.about_page.page).to_have_url("https://saucelabs.com/")
+        expect(pm.about_page.page).to_have_title("Sauce Labs: Cross Browser Testing, Selenium Testing & Mobile Testing")
+
+    def test_burger_menu_logout(self, pm, checkout):
+        pm.checkout_info_page.open_burger_menu()
+        expect(pm.checkout_info_page.burger_menu_logout).to_have_attribute("href","#")
+        pm.checkout_info_page.logout()
+        expect(pm.login_page.page).to_have_url("https://www.saucedemo.com/")
+        expect(pm.login_page.page).to_have_title("Swag Labs")
+
+    def test_cancel_checkout(self, pm, checkout):
+        pm.checkout_info_page.cancel_checkout()
+        expect(pm.cart_page.page).to_have_url("https://www.saucedemo.com/cart.html")
+
+    #TODO: move clicking to Base Page
+    def test_open_cart(self, pm, checkout):
+        pm.checkout_info_page.shopping_cart.click()
+        expect(pm.cart_page.page).to_have_url("https://www.saucedemo.com/cart.html")
+
+    def test_open_checkout_overview(self, pm, checkout):
+        pm.checkout_info_page.continue_checkout()
+        pass
+
+    def test_shipping_info_required(self, pm, checkout):
+        pass
+
+    def test_first_name_required(self, pm, checkout):
+        pass
+
+    def test_last_name_required(self, pm, checkout):
+        pass
+
+    def test_postal_code_required(self, pm, checkout):
+        pass
+
+    def test_last_name_postal_code_required(self, pm, checkout):
+        pass
+    
+    def test_first_name_postal_code_required(self, pm, checkout):
+        pass
